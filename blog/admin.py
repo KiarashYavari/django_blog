846e00ef4b9f5eefb473fc09c28f57f953af2b50
KiarashYavari <kiarash996@gmail.com>
2021-11-18 12:05:06 +0330

    add tow actions for articles in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
 from django.utils import timezone
 from blog.models import Article, Categories
+from django.utils.translation import ngettext
+from django.contrib import messages
 
 # Register your models here.
+@admin.action(description='انتشار مقالات')
+def make_published(modeladmin, request, queryset):
+    updated=queryset.update(Status='p')
+    modeladmin.message_user(request, ngettext(
+            '%d  مقاله منتشر شد',
+            '%d مقاله منتشر شدند ',
+            updated,
+        ) % updated, messages.SUCCESS)
+
+
+
+@admin.action(description='پیش نویس کردن مقالات')
+def make_draft(modeladmin, request, queryset):
+    updated=queryset.update(Status='d')
+    modeladmin.message_user(request, ngettext(
+            '%d  مقاله پیش نویس  شد',
+            '%d مقاله پیش نویس  شدند ',
+            updated,
+        ) % updated, messages.SUCCESS)
+
 class CategoriesAdmin(admin.ModelAdmin):
     list_display = ('Position','Title', 'Slug', 'Parent','Status')
     list_filter = (['Status'])
@@ -20,6 +42,7 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ('Title', 'Description')
     prepopulated_fields = {"Slug": ("Title",)}
     ordering = ('-Status', '-Published')
+    actions = [make_published, make_draft]
     
     def Category_str(self,cat_obj):
         return "، ".join([Category.Title for Category in cat_obj.enabled_categories()])
