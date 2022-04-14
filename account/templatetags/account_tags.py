f1f3f0e36d68fd6be0a67df8410b957b0547e6a3
KiarashYavari <kiarash996@gmail.com>
2022-04-14 10:20:48 +0430

    change profile link with custom template tags

--- account/templatetags/account_tags.py
+++ account/templatetags/account_tags.py
@@ -3,10 +3,11 @@ register = template.Library()
 
 
 @register.inclusion_tag('registration/partials/active_navbar.html')
-def active_navbar(request, link_name, content):
+def active_navbar(request, link_name, icon_classes, content):
     return {
         "request":request,
         "link_name":link_name,
         "link":"account:{}".format(link_name),
+        "icon_classes": icon_classes,
         "content":content,
     }
